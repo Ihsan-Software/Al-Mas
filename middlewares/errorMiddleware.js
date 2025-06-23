4ca7904a9da328e85a2c709832f059c0dc0c97e0
devMU19 <muhammedarsalan1992@gmail.com>
2025-06-24 02:13:10 +0300

    fix production error

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,5 +1,38 @@
 const ApiError = require('../utils/apiError');
 
+/* Error Function From Mongoose...
+    1) Invalid ID Format like rrrrrrr or fffffffff */
+const handleCastErrorDB = err =>{
+
+    const message = `Invalid ${err.path}: ${err.value}.`;
+    return new ApiError(message, 400);
+}
+
+//  2) Duplicate Fields Like Same Name
+const handleDuplicateFieldErrorDB = err =>{
+    const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+    const message = `Duplicate Fields Value: ${value}, please use other value.`;
+    return new ApiError(message, 400);
+}
+
+//  3) Validation Error Like Short Name 
+const handleValidationErrorDB = err =>{
+    const errors = Object.values(err.errors).map(err =>err.message);
+
+    const message = `Invalid Input Data In: ${errors.join('. ')}`;
+    return new ApiError(message, 400);
+}
+// End Error Function From Mongoose... 
+
+/* Error From JWT
+    1) Invaed Token */
+
+    const handleJWTError = err=> new ApiError('Invalid token, please log in again!',401)
+
+    // Expires Token
+    const handleTokenExpiredError = err=> new ApiError('Expire token, please log in again!',401)
+
+
 const sendErrorForDev = (err, res) =>
   res.status(err.statusCode).json({
     status: err.status,
@@ -27,25 +60,31 @@ const sendErrorForDev = (err, res) =>
         });
     }
 }
-
-const handleJwtInvalidSignature = () =>
-  new ApiError('Invalid token, please login again..', 401);
-
-const handleJwtExpired = () =>
-  new ApiError('Expired token, please login again..', 401);
-
+ 
 const globalError = (err, req, res, next) => {
   console.log('Welcome In Global Error Handling Middleware Function...💥');
 
   err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error...';
+  err.status = err.status || 'error...💥';
   if (process.env.NODE_ENV === 'development') {
     sendErrorForDev(err, res);
-  } else {
-    if (err.name === 'JsonWebTokenError') err = handleJwtInvalidSignature();
-    if (err.name === 'TokenExpiredError') err = handleJwtExpired();
-    sendErrorForProd(err, res);
-  }
+  } 
+  else if(process.env.NODE_ENV === 'production'){
+
+        // DB Error
+        if(err.name === 'CastError')
+            err = handleCastErrorDB(err);
+        if(err.code === 11000)
+            err = handleDuplicateFieldErrorDB(err);
+        if(err.name === 'ValidationError')
+            err = handleValidationErrorDB(err);
+        // JWT Error
+        if(err.name === 'JsonWebTokenError')
+            err = handleJWTError(err);
+        if(err.name === 'TokenExpiredError')
+            err = handleTokenExpiredError(err);
+        sendErrorForProd(err, res);
+    }
 };
 
 module.exports = globalError;
