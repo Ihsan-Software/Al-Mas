1cb9bfb0110d31856996ef6f5556ad4939996318
devMU19 <muhammedarsalan1992@gmail.com>
2025-08-20 14:35:14 +0300

    fix manager to admin

--- models/userInfoModel.js
+++ models/userInfoModel.js
@@ -21,7 +21,7 @@ const userInfoSchema = new mongoose.Schema(
     phone: String,
     role: {
         type: String,
-        enum: ["manager", "employee"],
+        enum: ["admin", "employee"],
         default: "employee",
     }
 },
